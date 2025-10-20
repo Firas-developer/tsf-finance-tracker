462d120559f9c47bfdffc9898f7d7dd3b4d2bc0d
firas-07 <muhammadfiras038@gmail.com>
2025-10-20 11:33:04 +0530

    updated allowed origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,7 +25,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://finance-tracker-app-tau-ruddy.vercel.app/"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
